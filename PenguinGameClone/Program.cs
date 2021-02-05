e5c0e32ac19d6782a30f0b4f948e53c5455af29e
kdm1jkm <kdm1jkmm@gmail.com>
2021-02-05 21:21:25 +0900

    init.

---
+++ PenguinGameClone/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PenguinGameClone
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
